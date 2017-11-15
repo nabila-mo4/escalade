1cfb0b7242dc008cfdfdda9cd7c02e93add6cdfe
nabila-mo4 <32497695+nabila-mo4@users.noreply.github.com>
2017-11-15 15:39:00 +0100

    Create Site.java

---
+++ src/com/escalade/beans/Site.java
@@ -0,0 +1,43 @@
+package com.escalade.beans;
+
+public class Site {
+	private String nom;
+	private String emplacementGeo;
+	private String type;
+	private int hauteur;
+	private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getEmplacementGeo() {
+		return emplacementGeo;
+	}
+	public void setEmplacementGeo(String emplacementGeo) {
+		this.emplacementGeo = emplacementGeo;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getHauteur() {
+		return hauteur;
+	}
+	public void setHauteur(int hauteur) {
+		this.hauteur = hauteur;
+	}
+	
+	
+
+}
