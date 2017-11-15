60abc0b3feda4a6561511beb5667c1490fdc4991
nabila-mo4 <32497695+nabila-mo4@users.noreply.github.com>
2017-11-15 15:38:23 +0100

    Create Commentaire.java

---
+++ src/com/escalade/beans/Commentaire.java
@@ -0,0 +1,28 @@
+package com.escalade.beans;
+
+import java.util.Date;
+
+public class Commentaire {
+		private Long id;
+	
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		private Date datePublication;
+
+		public Date getDatePublication() {
+			return datePublication;
+		}
+
+		public void setDatePublication(Date datePublication) {
+			this.datePublication = datePublication;
+		}
+		
+		
+
+}
